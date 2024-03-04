5967243069898f2398b56b2c07c8165329a22502
AyoubMarkhouss <ayoub.markhouss@gmail.com>
2024-03-04 20:25:20 +0100

    contact page

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Image from "next/image";
 const Steps = () => {
   return (
     <div className="z-30 relative px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
@@ -200,7 +200,8 @@ const Steps = () => {
           </div>
         </div>
         <div className="relative md:mt-10">
-          <img
+          <Image
+            about="zz"
             className="inset-0 object-cover object-bottom w-full rounded shadow-lg h-80 lg:absolute lg:h-96"
             src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
             alt=""
